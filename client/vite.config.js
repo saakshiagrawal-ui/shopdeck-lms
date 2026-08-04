97c3b3a69886f0da1204a301e483a88b0ed602fb
saakshiagrawal-ui <saakshi.agrawal@blitzscale.co>
2026-08-04 11:30:55 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
   build: {
-    outDir: '../server/client/dist',
-    emptyOutDir: true
+    outDir: 'dist'
   }
 })
